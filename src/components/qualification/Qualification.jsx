7281a76ca3b020630520544f4ce80e3eb08a2d8f
Sayantan Chowdhury <135856824+Sayan-2054@users.noreply.github.com>
2026-02-14 21:12:39 +0530

    Update Qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -62,17 +62,17 @@ const Qualification = () => {
 
                 <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                      <div className="qualification__data">
+                         <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div> 
+
                         <div>
                             <h3 className="qualification__title">Software Development Engineer - Intern</h3>
                             <span className="qualification__subtitle">State Street Corporation</span>
                             <div className="qualification__calendar">
                                 <i className="uil uil-calendar-alt"></i> Jan 2026 - Present
                             </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
                         </div>
                     </div>
 
